35b499126240422211ebda50811c2b6060ccedf6
Daniel Gibbs <me@danielgibbs.co.uk>
2023-01-19 13:49:14 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,26 +5,11 @@ terraform {
       source  = "linode/linode"
       version = "1.29.4"
     }
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "3.32.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9.1"
-    }
   }
-}
-
-provider "time" {
 }
 
 provider "linode" {
   token = var.linode_token
-}
-
-provider "cloudflare" {
-  api_token = var.cloudflare_token
 }
 
 # Ensure Linodes Instances Exist.
@@ -40,38 +25,7 @@ resource "linode_instance" "linode-server" {
   watchdog_enabled = "true"
 }
 
-# Ensure Cloudflare A Record Exists.
-# 60 Second ttl setup to allow rDNS to work
-resource "cloudflare_record" "cloudflare-dns" {
-  count   = length(linode_instance.linode-server)
-  zone_id = var.cloudflare_zone_id
-  name    = var.server_name[count.index]
-  value   = linode_instance.linode-server[count.index].ip_address
-  type    = "A"
-  proxied = false
-  ttl     = 60
-  allow_overwrite = true
-}
-
-# Wait 60 Seconds to allow A Record to update.
-resource "time_sleep" "wait_60_seconds" {
-  depends_on      = [cloudflare_record.cloudflare-dns]
-  create_duration = "60s"
-}
-
-# Ensure Linode Instance rDNS exists.
-# rDNS has to check existing A record exists and matches
-resource "linode_rdns" "my_rdns" {
-  depends_on = [time_sleep.wait_60_seconds]
-  count      = length(linode_instance.linode-server)
-  address    = linode_instance.linode-server[count.index].ip_address
-  rdns       = cloudflare_record.cloudflare-dns[count.index].hostname
-}
-
 output "linode_ip_addr" {
   value = linode_instance.linode-server[*].ip_address
 }
 
-output "cloudflare_hostname" {
-  value = cloudflare_record.cloudflare-dns[*].hostname
-}
